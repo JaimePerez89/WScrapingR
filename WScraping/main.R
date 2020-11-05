5248ad5fdc9658f5272adc95bd2757053c2474ed
Jaime Perez <ordieres_jaime@hotmail.com>
2020-11-06 00:55:04 +0100

    Añadidas funciones script y archivos finales de ejemplo

--- WScraping/main.R
+++ WScraping/main.R
@@ -1,3 +1,59 @@
-# En este fichero se realizará la carga de las funciones tanto del script "Scrap.R" como del script "clean_df.R"
+# Hacemos accesibles las funciones creadas
+source("./WScraping/scrap.R")
 
-source()
+
+# Cargamos las librerías. En caso de no tenerlas instaladas, se descargarán
+PACKAGES <- c("dplyr", "rvest", "ggplot2", "tibble", "knitr", "stringr")
+inst <- match(PACKAGES, .packages(all=TRUE))
+need <- which(is.na(inst))
+if (length(need) > 0) install.packages(PACKAGES[need], repos = "http://cran.us.r-project.org")
+lapply(PACKAGES, require, character.only=T)
+
+
+# Llamamos la función scrap_top30 para cada una de los géneros disponibles
+generos_disponibles <- c("AC", "AN", "AV", "BE", "C-F", "F-N", "CO", "DR", "FAN", "INF", "INT", "MU", "RO", "TE", "TH", "WE")
+BD_peliculas_top <- data.frame() # Creo un data.frame vacio sobre el que añadir filas
+
+for (i in generos_disponibles){
+        # Llamo a la función
+        BD_peliculas_scrap <- scrap_top30(i)
+        # Añado filas a base de datos
+        BD_peliculas_top <- bind_rows(BD_peliculas_top, BD_peliculas_scrap)
+        # Meto un retraso de 1 segundo para no saturar el servidor consultado
+        Sys.sleep(1)
+}
+
+
+# Creamos un csv con los resultados
+write.csv(BD_peliculas_top, file = "./WScraping/BD_inicial_top30.csv", row.names = FALSE)
+
+
+# Para cada uno de los id obtenidos, se extrae la información relacionada con la película
+# for (j in BD_peliculas_top$id_pelicula){
+#         target_page <- read_html(target_url)
+#         
+# }
+
+
+
+
+# Ejemplo para no ejecutarlo todo junto
+id_url_extract <- BD_peliculas_top$id_pelicula[4]
+
+BD_id_prueba <- scrap_film(id_url = id_url_extract)
+BD_id_prueba
+
+BD_peliculas_full <- data.frame()
+# COjo solo 5 peliculas
+for (m in BD_peliculas_top$id_pelicula[1:8]){
+        BD_id_scrap <- scrap_film(id_url = m)
+        BD_peliculas_full <- bind_rows(BD_peliculas_full, BD_id_scrap)
+        Sys.sleep(1)
+}
+
+BD_peliculas_full
+
+
+BD_peliculas_final <- right_join(BD_peliculas_top, BD_peliculas_full, by = "id_pelicula")
+
+write.csv2(BD_peliculas_final, file = "./WScraping/BD_peliculas.csv", row.names = FALSE)
--- WScraping/scrap.R
+++ WScraping/scrap.R
@@ -1,1 +1,228 @@
-# En este fichero se creará una función que en base a una url, extraiga la información que demandemos
+# El fichero contiene 2 funciones:
+#       scrap_top30: extrae las 30 películas del género input
+#       scrap_film: dado el link con el id de una película extrae la información disponible
+
+
+scrap_top30 <- function(genre){
+        # Función que extrae el top30 de las películas del género solicitado
+        # param genre: character (ej. AC)
+        #
+        # Posibles valores de entrada:
+        #       - Acción: AC
+        #       - Animación: AN
+        #       - Aventuras: AV
+        #       - Bélico: BE
+        #       - Ciencia ficción: C-F
+        #       - Cine negro: F-N
+        #       - Comedia: CO
+        #       - Drama: DR
+        #       - Fantástico: FN
+        #       - Infantil: INF
+        #       - Intriga: INT
+        #       - Musical: MU
+        #       - Romance: RO
+        #       - Terror: TE
+        #       - Thriller: TH
+        #       - Western: WE
+        
+        posibles_generos <- c("AC", "AN", "AV", "BE", "C-F", "F-N", "CO", "DR", "FAN", "INF", "INT", "MU", "RO", "TE", "TH", "WE")
+        
+        if (!(genre %in% posibles_generos)){
+                mens_error <- "No existe el genero en la plataforma"
+                return(mens_error)
+        }
+        
+        url_comun_inicial <- "https://www.filmaffinity.com/es/topgen.php?genre="
+        url_comun_final <- "&fromyear=&toyear=&country=&nodoc&notvse"
+        pag_a_consultar <- paste(url_comun_inicial, genre, url_comun_final, sep = "")
+        
+        target_page <- read_html(pag_a_consultar)
+        
+        nombre_path <- target_page %>%
+                html_nodes(xpath = '//*[@id="top-movies"]')
+        
+        posicion <- nombre_path %>%
+                html_nodes("li.position") %>%
+                html_text(trim = TRUE)
+        
+        nombre <- nombre_path %>%
+                html_nodes("li.content") %>%
+                html_nodes("div.mc-title") %>%
+                html_text(trim = TRUE)
+        
+        id_pelicula <- nombre_path %>%
+                html_nodes("li.content") %>%
+                html_nodes("div.mc-poster") %>%
+                html_nodes("a") %>%
+                html_attr("href")
+        
+        BD_peliculas <- data.frame(
+                "genero" = genre, 
+                "posicion" = posicion,
+                "nombre" = nombre,
+                "id_pelicula" = id_pelicula
+        )
+        
+        return(BD_peliculas)
+}
+
+
+scrap_film <- function(id_url){
+        # Función que extrae la información de la página de la película input
+        # param id_url: character (ej. https://www.filmaffinity.com/es/film811454.html)
+        
+        target_page <- read_html(id_url)
+        
+        results <- data.frame("id_pelicula" = id_url)
+        
+        
+        cabecera <- target_page %>%
+                html_node("dl.movie-info") %>%
+                html_nodes("dt") %>%
+                html_text(trim = TRUE)
+        
+        info <- target_page %>%
+                html_node("dl.movie-info") %>%
+                html_nodes("dd") %>%
+                html_text(trim = TRUE)
+        
+        BD_pelicula_id <- data.frame(cabecera, info)
+        
+        # Titulo original
+        titulo_original <- BD_pelicula_id %>% filter(cabecera == "Título original") %>% pull(info)
+        if(length(titulo_original) == 0){
+                titulo_original <- NA
+        }
+        results <- cbind(results, "titulo_original" = titulo_original)
+        
+        
+        # Año
+        anio <- BD_pelicula_id %>% filter(cabecera == "Año") %>% pull(info)
+        if(length(anio) == 0){
+                anio <- NA
+        }
+        results <- cbind(results, "anio" = anio)
+        
+        # Duración
+        duracion <- BD_pelicula_id %>% filter(cabecera == "Duración") %>% pull(info)
+        if(length(duracion) == 0){
+                duracion <- NA
+        }
+        results <- cbind(results, "duracion" = duracion)
+        
+        # País
+        pais <- BD_pelicula_id %>% filter(cabecera == "País") %>% pull(info)
+        if(length(pais) == 0){
+                pais <- NA
+        }
+        results <- cbind(results, "pais" = pais)
+        
+        # Dirección
+        direccion <- BD_pelicula_id %>% filter(cabecera == "Dirección") %>% pull(info)
+        if(length(direccion) == 0){
+                direccion <- NA
+        }
+        results <- cbind(results, "direccion" = direccion)
+        
+        # Guion
+        guion <- BD_pelicula_id %>% filter(cabecera == "Guion") %>% pull(info)
+        if(length(guion) == 0){
+                guion <- NA
+        }
+        results <- cbind(results, "guion" = guion)
+        
+        # Música
+        musica <- BD_pelicula_id %>% filter(cabecera == "Música") %>% pull(info)
+        if(length(musica) == 0){
+                musica <- NA
+        }
+        results <- cbind(results, "musica" = musica)
+        
+        # Fotografía
+        fotografia <- BD_pelicula_id %>% filter(cabecera == "Fotografía") %>% pull(info)
+        if(length(fotografia) == 0){
+                fotografia <- NA
+        }
+        results <- cbind(results, "fotografia" = fotografia)
+        
+        # Reparto
+        reparto <- BD_pelicula_id %>% filter(cabecera == "Reparto") %>% pull(info)
+        if(length(reparto) == 0){
+                reparto <- NA
+        }
+        results <- cbind(results, "reparto" = reparto)
+        
+        # Productora
+        productora <- BD_pelicula_id %>% filter(cabecera == "Productora") %>% pull(info)
+        if(length(productora) == 0){
+                productora <- NA
+        }
+        results <- cbind(results, "productora" = productora)
+        
+        # Género
+        genero <- BD_pelicula_id %>% filter(cabecera == "Género") %>% pull(info)
+        if(length(genero) == 0){
+                genero <- NA
+        }
+        results <- cbind(results, "genero" = genero)
+        
+        # Sinopsis
+        sinopsis <- BD_pelicula_id %>% filter(cabecera == "Sinopsis") %>% pull(info)
+        if(length(sinopsis) == 0){
+                sinopsis <- NA
+        }
+        results <- cbind(results, "sinopsis" = sinopsis)
+        
+        # Puntuación media
+        puntuacion_media <- target_page %>% 
+                html_nodes(xpath = '//*[@id="movie-rat-avg"]') %>%
+                html_text(trim = TRUE)
+        results <- cbind(results, "puntuacion_media" = puntuacion_media)
+        
+        # Mejor puntuación
+        mejor_puntuacion <- target_page %>% 
+                html_nodes(xpath = '//*[@id="rat-avg-container"]/meta[1]') %>%
+                html_attr("content")
+        results <- cbind(results, "mejor_puntuacion" = mejor_puntuacion)
+        
+        # Peor puntuación
+        peor_puntuacion <- target_page %>% 
+                html_nodes(xpath = '//*[@id="rat-avg-container"]/meta[2]') %>%
+                html_attr("content")
+        results <- cbind(results, "peor_puntuacion" = peor_puntuacion)
+        
+        # Votos
+        votos <- target_page %>% 
+                html_nodes(xpath = '//*[@id="movie-count-rat"]/span') %>%
+                html_text(trim = TRUE)
+        results <- cbind(results, "votos" = votos)
+        
+        # Número de críticas
+        num_criticas <- target_page %>% 
+                html_nodes(xpath = '//*[@id="rat-avg-container"]/meta[3]') %>%
+                html_attr("content")
+        results <- cbind(results, "num_criticas" = num_criticas)
+        
+        # 
+        # results <- data.frame("id_pelicula" = id,
+        #                      "titulo_original" = titulo_original,
+        #                      "anio" = anio,
+        #                      "duracion" = duracion,
+        #                      "puntuacion_media" = puntuacion_media,
+        #                      "mejor_puntuacion" = mejor_puntuacion,
+        #                      "peor_puntuacion" = peor_puntuacion,
+        #                      "votos" = votos,
+        #                      "num_criticas" = num_criticas,
+        #                      "pais" = pais,
+        #                      "direccion" = direccion,
+        #                      "guion" = guion,
+        #                      "musica" = musica,
+        #                      "fotografia" = fotografia,
+        #                      "reparto" = reparto,
+        #                      "productora" = productora,
+        #                      "genero" = genero,
+        #                      "sinopsis" = sinopsis)
+        
+        return(results)
+        
+}
